05e3268e8bfd6fb4635cb9c15a206ea370fd57ce
rafaelcostaleite <rafaelcostaleite@gmail.com>
2021-08-20 23:45:30 -0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+lst = ['Java', 'Python', 'C', 'C++', 'JavaScript', 'Swift', 'Go']
+
+# Calling DataFrame
+dframe = pd.DataFrame(lst)
+print(dframe)
+print("Meu script do Python")
