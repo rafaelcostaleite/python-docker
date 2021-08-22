8c23ca1b13582c1cbc40dd8576ac02573fe778ba
rafaelcostaleite <rafaelcostaleite@gmail.com>
2021-08-22 01:07:38 -0300

    versao 3

--- script.py
+++ script.py
@@ -5,4 +5,4 @@ lst = ['Java', 'Python', 'C', 'C++', 'JavaScript', 'Swift', 'Go']
 # Calling DataFrame
 dframe = pd.DataFrame(lst)
 print(dframe)
-print("Meu script do Python 2")
+print("Meu script do Python 3")
